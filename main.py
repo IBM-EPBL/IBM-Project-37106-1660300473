1907190d0f60c4ea91dde9144c2749fe877a451d
Preethi-R25 <67852540+Preethi-R25@users.noreply.github.com>
2022-10-05 22:52:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import random 
+while(True): 
+    temp=random.randint(10,99) 
+    humid=random.randint(10,99) 
+    print("current temperature:",temp) 
+    print("current humidity:",humid,"%") 
+    temp_ref=35 
+    humid_ref=35 
+    if temp>temp_ref and humid<humid_ref: 
+        print("Sound Alarm On..!") 
+    else: 
+        print("Sound off..!") 
+        break s
